fb78141a4b490a8b66e4751720738beb477ab6f3
Dela Cruz, Luis Adam D <luisadamdelacruz8@gmail.com>
2024-05-31 10:39:07 +0800

    Add files via upload

---
+++ style.js.js
@@ -0,0 +1,11 @@
+function changecolor() {
+    document.getElementById('JavaScript').style.color = "blue";
+  }
+  
+  function changecolor2() {
+    document.getElementById('JavaScript').style.color = "red";
+  }
+  
+  function changeimage() {
+    document.getElementById('Joke').src = "exercise/image.gif";
+  }
